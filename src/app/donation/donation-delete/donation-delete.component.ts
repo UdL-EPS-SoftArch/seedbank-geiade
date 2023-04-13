b96a11b12273460bd4df793e957590e0e53fec2d
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-13 16:04:58 +0200

    Donation Delete Component Created

---
+++ src/app/donation/donation-delete/donation-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-delete',
+  templateUrl: './donation-delete.component.html',
+  styleUrls: ['./donation-delete.component.css']
+})
+export class DonationDeleteComponent {
+
+}

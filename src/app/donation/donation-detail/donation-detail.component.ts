7bf105295931689a6d2d55fb342c3055a2bdbacc
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-13 16:05:18 +0200

    Donation Detail Component Created

---
+++ src/app/donation/donation-detail/donation-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-detail',
+  templateUrl: './donation-detail.component.html',
+  styleUrls: ['./donation-detail.component.css']
+})
+export class DonationDetailComponent {
+
+}

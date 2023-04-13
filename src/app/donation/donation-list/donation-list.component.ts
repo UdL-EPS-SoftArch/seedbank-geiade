f3687a8fea260dca0d641c7f10ea4398c1658845
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-13 16:56:52 +0200

    Donation list component html and ts

--- src/app/donation/donation-list/donation-list.component.ts
+++ src/app/donation/donation-list/donation-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Donation } from '../donation';
+import { DonationService } from '../donation.service';
+import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
 
 @Component({
   selector: 'app-donation-list',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./donation-list.component.css']
 })
 export class DonationListComponent {
+  public donations: Donation[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalDonations = 0;
+
+  constructor(
+    public router: Router,
+    private donationService: DonationService) {
+  }
+
+  ngOnInit(): void {
+    debugger;
+    this.donationService.getPage({ pageParams:  { size: this.pageSize }, sort: { id: 'ASC' } }).subscribe(
+        (page: PagedResourceCollection<Donation>) => {
+          this.donations = page.resources;
+          this.totalDonations = page.totalElements;
+        });
+  }
+
+  changePage(): void {
+    this.donationService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { id: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Donation>) => this.donations = page.resources);
+  }
+
+  detail(donation: Donation): void {
+    this.router.navigate(['donations', donation.id]);
+  }
 
 }

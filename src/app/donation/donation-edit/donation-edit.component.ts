98ab4d27083b9a229871d929c553b05eff13305d
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-13 16:05:34 +0200

    Donation Edit Component Created

---
+++ src/app/donation/donation-edit/donation-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-edit',
+  templateUrl: './donation-edit.component.html',
+  styleUrls: ['./donation-edit.component.css']
+})
+export class DonationEditComponent {
+
+}

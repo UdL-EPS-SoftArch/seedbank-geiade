9f92ced7c3ecc9eaf817b14cb44c4db2555e501f
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-18 18:14:03 +0200

    Donation edit component

--- src/app/donation/donation-edit/donation-edit.component.ts
+++ src/app/donation/donation-edit/donation-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Donation } from '../donation';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DonationService } from '../donation.service';
 
 @Component({
   selector: 'app-donation-edit',
   templateUrl: './donation-edit.component.html',
   styleUrls: ['./donation-edit.component.css']
 })
-export class DonationEditComponent {
+export class DonationEditComponent implements OnInit{
+  public donation: Donation = new Donation();
+  public donationId;
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private donationService: DonationService) {
+  }
+
+  ngOnInit(): void {
+    this.donationId = this.route.snapshot.paramMap.get('id');
+    this.donationService.getResource(this.donationId).subscribe(
+      (donation: Donation) => this.donation = donation );
+  }
+
+  onSubmit(): void {
+    this.donationService.patchResource(this.donation).subscribe(
+      (patchedDonation: Donation) => {
+        this.router.navigate([patchedDonation.uri]);
+      });
+  }
 }

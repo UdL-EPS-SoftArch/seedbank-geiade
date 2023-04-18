7bc22688bbdee77e32e66e5d776eb6b5bc8e7d51
Ilyass Mahdiyan <ilyassmah00@gmail.com>
2023-04-18 18:17:44 +0200

    Donation delete component

--- src/app/donation/donation-delete/donation-delete.component.ts
+++ src/app/donation/donation-delete/donation-delete.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Donation } from '../donation';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DonationService } from '../donation.service';
 
 @Component({
   selector: 'app-donation-delete',
   templateUrl: './donation-delete.component.html',
   styleUrls: ['./donation-delete.component.css']
 })
-export class DonationDeleteComponent {
+export class DonationDeleteComponent implements OnInit{
+  public donation: Donation = new Donation();
+  private id: string;
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private donationService: DonationService) {
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.donationService.getResource(this.id).subscribe(
+      donation => this.donation = donation);
+  }
+
+  delete(): void {
+    this.donationService.deleteResource(this.donation).subscribe(
+      () => {
+        this.router.navigate(['']);
+      });
+  }
 }

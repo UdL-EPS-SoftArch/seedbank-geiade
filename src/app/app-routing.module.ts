4a946c1ba2e0dca5f31fbcf8a5565f0f32e681b1
Sergi <sergiedits@gmail.com>
2023-04-19 16:03:23 +0200

    Added imports and paths for take component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserRegisterComponent } from './user/user-register/user-register.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import { TakeRegisterComponent } from './take/take-register/take-register.component';
+import { TakeEditComponent } from './take/take-edit/take-edit.component';
+import { TakeDeleteComponent } from './take/take-delete/take-delete.component';
+import { TakeDetailComponent } from './take/take-detail/take-detail.component';
+import { TakeListComponent } from './take/take-list/take-list.component';
 
 const routes: Routes = [
   { path: 'users/create', component: UserRegisterComponent},
@@ -15,6 +20,11 @@ const routes: Routes = [
   { path: 'users/:id/edit', component: UserEditComponent, canActivate: [LoggedInGuard]},
   { path: 'users/:id', component: UserDetailComponent, canActivate: [LoggedInGuard]},
   { path: 'users', component: UserListComponent, canActivate: [LoggedInGuard]},
+  { path: 'takes/create', component: TakeRegisterComponent, canActivate: [LoggedInGuard]},
+  { path: 'takes/:id/delete', component: TakeDeleteComponent, canActivate: [LoggedInGuard]},
+  { path: 'takes/:id/edit', component: TakeEditComponent, canActivate: [LoggedInGuard]},
+  { path: 'takes/:id', component: TakeDetailComponent, canActivate: [LoggedInGuard]},
+  { path: 'takes', component: TakeListComponent, canActivate: [LoggedInGuard]},
   { path: 'about', component: AboutComponent},
   { path: '404', component: NotFoundComponent},
   { path: '', redirectTo: 'about', pathMatch: 'full'},

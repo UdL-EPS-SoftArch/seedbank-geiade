2aae62cd007fe90219fc7c262110e1575f8b26da
sala1714 <pepesalamarques@gmail.com>
2023-04-25 18:41:41 +0200

    Propagator error solving

--- src/app/propagator/propagator-list/propagator-list.component.ts
+++ src/app/propagator/propagator-list/propagator-list.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Propagator } from '../propagator';
+import { PropagatorService } from '../propagator.service';
+import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
 
 @Component({
   selector: 'app-propagator-list',
   templateUrl: './propagator-list.component.html',
   styleUrls: ['./propagator-list.component.css']
 })
-export class PropagatorListComponent {
+export class propagatorListComponent {
+  public propagators: Propagator[] = [];
+  public pageSize = 5;
+  public page = 1;
+  public totalpropagators = 0;
+
+  constructor(
+    public router: Router,
+    private propagatorService: PropagatorService) {
+  }
+
+  ngOnInit(): void {
+    debugger;
+    this.propagatorService.getPage({ pageParams:  { size: this.pageSize }, sort: { id: 'ASC' } }).subscribe(
+        (page: PagedResourceCollection<Propagator>) => {
+          this.propagators = page.resources;
+          this.totalpropagators = page.totalElements;
+        });
+  }
+
+  changePage(): void {
+    this.propagatorService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { id: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Propagator>) => this.propagators = page.resources);
+  }
+
+  detail(propagator: Propagator): void {
+    this.router.navigate(['propagators', propagator.id]);
+  }
 
 }
--- src/app/propagator/propagator.ts
+++ src/app/propagator/propagator.ts
@@ -1,19 +1,23 @@
+import { Authority } from '../login-basic/authority';
 import { HateoasResource, Resource } from "@lagoshny/ngx-hateoas-client";
-import { User } from "../login-basic/user";
-import { Collection } from "cypress/types/lodash";
 
-@HateoasResource('propagators')
-export class Propagator extends Resource{
-    uri: string;
-    id: string;
-    email: string;
-    password: string;
-    passwordReset: boolean;
-    role: string;
-
-    constructor(values: object = {}) {
+ @HateoasResource('propagator')
+    export class Propagator extends Resource {
+      id: string;
+      username: string;
+      email: string;
+      authorities: Authority[] = [];
+      authorization = '';
+      password = '';
+      passwordReset = false;
+      uri: string;
+    
+      constructor(values: object = {}) {
         super();
         Object.assign(this as any, values);
+      }
+    
+      getRoles(): string[] {
+        return this.authorities.map(a => a.authority.split('_')[1].toLowerCase());
+      }
     }
-
-}

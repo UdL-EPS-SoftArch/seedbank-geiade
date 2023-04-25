0ef9b7081bbc3e0385f84ebfe1c6d2bb7914cb2c
sala1714 <pepesalamarques@gmail.com>
2023-04-25 18:24:12 +0200

    propagator-list initial config

--- src/app/propagator/propagator-edit/propagator-edit.component.ts
+++ src/app/propagator/propagator-edit/propagator-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Propagator } from '../propagator';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PropagatorService } from '../propagator.service';
 
 @Component({
   selector: 'app-propagator-edit',
   templateUrl: './propagator-edit.component.html',
   styleUrls: ['./propagator-edit.component.css']
 })
-export class PropagatorEditComponent {
+export class propagatorEditComponent implements OnInit{
+  public propagator: Propagator = new Propagator();
+  public propagatorId;
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private propagatorService: PropagatorService) {
+  }
+
+  ngOnInit(): void {
+    this.propagatorId = this.route.snapshot.paramMap.get('id');
+    this.propagatorService.getResource(this.propagatorId).subscribe(
+      (propagator: Propagator) => this.propagator = propagator );
+  }
+
+  onSubmit(): void {
+    this.propagatorService.patchResource(this.propagator).subscribe(
+      (patchedpropagator: Propagator) => {
+        this.router.navigate([patchedpropagator.uri]);
+      });
+  }
 }

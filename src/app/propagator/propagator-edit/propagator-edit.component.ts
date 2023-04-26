9f15266f79c0cd9ace1df0f06a8b517ceb4ba61a
sala1714 <pepesalamarques@gmail.com>
2023-04-26 17:57:37 +0200

    Propagator getUserName error solving

--- src/app/propagator/propagator-edit/propagator-edit.component.ts
+++ src/app/propagator/propagator-edit/propagator-edit.component.ts
@@ -1,32 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { Propagator } from '../propagator';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
 import { PropagatorService } from '../propagator.service';
+import { Propagator } from '../propagator';
 
 @Component({
   selector: 'app-propagator-edit',
-  templateUrl: './propagator-edit.component.html',
-  styleUrls: ['./propagator-edit.component.css']
+  templateUrl: './propagator-edit.component.html'
 })
-export class PropagatorEditComponent implements OnInit{
+export class PropagatorEditComponent implements OnInit {
   public propagator: Propagator = new Propagator();
-  public propagatorId;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private propagatorService: PropagatorService) {
+              private propagatorService: PropagatorService,
+              private authenticationService: AuthenticationBasicService) {
   }
 
   ngOnInit(): void {
-    this.propagatorId = this.route.snapshot.paramMap.get('id');
-    this.propagatorService.getResource(this.propagatorId).subscribe(
+    const id = this.route.snapshot.paramMap.get('id');
+    this.propagatorService.getResource(id).subscribe(
       (propagator: Propagator) => this.propagator = propagator );
   }
 
   onSubmit(): void {
+    this.propagator.password = this.propagator.passwordReset ? this.propagator.password : undefined; // Don't edit if not a reset
     this.propagatorService.patchResource(this.propagator).subscribe(
       (patchedpropagator: Propagator) => {
-        this.router.navigate([patchedpropagator.uri]);
+        if (this.propagator.passwordReset) {
+          this.authenticationService.logout();
+          this.authenticationService.login(this.propagator.id, this.propagator.password).subscribe(
+            (propagator: Propagator) => this.router.navigate(['propagators', propagator.id]));
+        } else {
+          this.router.navigate(['propagators', patchedpropagator.id]);
+        }
       });
   }
+
+  getCurrentUserName(): string {
+    return this.authenticationService.getCurrentUser().id;
+  }
 }
+

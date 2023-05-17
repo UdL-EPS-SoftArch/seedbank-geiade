c847f2a878cb8875de717c29694a24347ba1e350
Robert Torres <robert_torres@protonmail.com>
2023-05-17 18:39:47 +0200

    Implemented batch component ts

--- src/app/batch/batch-add/batch-add.component.ts
+++ src/app/batch/batch-add/batch-add.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Batch } from '../batch';
+import { Router } from '@angular/router';
+import { BatchService } from '../batch.service';
+import { AuthenticationBasicService } from 'src/app/login-basic/authentication-basic.service';
+import { User } from 'src/app/login-basic/user';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-batch-add',
   templateUrl: './batch-add.component.html',
   styleUrls: ['./batch-add.component.css']
 })
-export class BatchAddComponent {
+export class BatchAddComponent implements OnInit{
+  public batch: Batch;
+  public user: User;
 
+  constructor(private router: Router,
+              private batchService: BatchService,
+              private userService: UserService,
+              private authenticationService: AuthenticationBasicService) {
+  }
+
+  ngOnInit(): void {
+    this.batch = new Batch();
+    this.userService.getResource(this.getCurrentUserName()).subscribe(
+      (user: User) => this.user = user );
+  }
+
+  onSubmit(): void {
+    this.batch.by = this.user;
+    this.batchService.createResource({ body: this.batch }).subscribe(
+      (batch) => {
+        this.router.navigate([batch.uri]);
+      });
+  }
+
+  onCancel(): void {
+    this.router.navigate(["/batchs"])
+  }
+
+  getCurrentUserName(): string {
+    return this.authenticationService.getCurrentUser().id;
+  }
 }
--- src/app/batch/batch-edit/batch-edit.component.ts
+++ src/app/batch/batch-edit/batch-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Batch } from '../batch';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BatchService } from '../batch.service';
 
 @Component({
   selector: 'app-batch-edit',
   templateUrl: './batch-edit.component.html',
   styleUrls: ['./batch-edit.component.css']
 })
-export class BatchEditComponent {
+export class BatchEditComponent implements OnInit{
+  public batch: Batch = new Batch();
+  public batchId;
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private batchService: BatchService) {
+  }
+
+  ngOnInit(): void {
+    this.batchId = this.route.snapshot.paramMap.get('id');
+    this.batchService.getResource(this.batchId).subscribe(
+      (batch: Batch) => this.batch = batch );
+  }
+
+  onSubmit(): void {
+    this.batchService.patchResource(this.batch).subscribe(
+      (patchedDonation: Batch) => {
+        this.router.navigate([patchedDonation.uri]);
+      });
+  }
 }
